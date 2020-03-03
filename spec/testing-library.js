e84e0dd42c0099a421b43644a0c0d9ba07bd1a9b
Paulo Azoia <pazoia.gomes@gmail.com>
2020-03-03 14:22:23 +0000

    Added testing-library

---
+++ spec/testing-library.js
@@ -0,0 +1,20 @@
+const describe = (desc, fn) => {
+  console.log(desc);
+  fn();
+};
+
+const it = (msg, fn) => describe(' ' + msg, fn);
+
+const matchers = (exp) => ({
+  toBe: (assertion) => {
+    if (exp === assertion) {
+      console.log('pass')
+      return true
+    } else {
+      console.log('fail')
+      return false
+    };
+  }
+});
+
+const expect = (exp) => matchers(exp);
