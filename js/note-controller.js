2f787fcf79e56568d19b348ac7330a22832def40
Liz Daly <liz7daly@gmail.com>
2020-03-04 16:34:03 +0000

    Test drive note controller

--- js/note-controller.js
+++ js/note-controller.js
@@ -1,3 +1,19 @@
 function NoteController(noteList) {
   this.noteList = noteList
+  this.noteList.createNote("Favourite drink: seltzer")
+  this.noteListView = new NoteListView(this.noteList)
 }
+
+NoteController.prototype = (function() {
+  function insertHTML() {
+    document.getElementById('app').innerHTML = this.noteListView.displayNoteView()
+    
+  }
+  return {
+    insertHTML: insertHTML
+  }
+})();
+
+noteList = new NoteList();
+noteController = new NoteController(noteList);
+
--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -1,13 +1,26 @@
 describe ("note controller", function() {
-  function NoteListDouble() {};
-  noteController = new NoteController(new NoteListDouble);
+  // function NoteListDouble() {
+  //   this.storedNotes = []
+  // };
+
+  // NoteListDouble.prototype = {
+  //   createNote: function(note) {
+  //     this.storedNotes.push({text: note})
+  //   },
+  //   displayList: function() {
+  //     return this.storedNotes
+  //   }
+  // }
+
+
+  noteController = new NoteController(new NoteList);
 
   it ("can take a note list as a parameter", function() {
-    expect(noteController.noteList).toBeInstanceOf(NoteListDouble)
+    expect(noteController.noteList).toBeInstanceOf(NoteList)
   })
 
   it("adds a note to the note list", function() {
     noteController.insertHTML();
-    expect(document.getElementById('app').innerHTML).toBe('<ul></div></li>Favourite drink: seltzer</div></li></ul>')
+    expect(document.getElementById('app').innerHTML).toBe('<ul><li><div>Favourite drink: seltzer</div></li></ul>')
   })
 });
